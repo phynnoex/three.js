32d29e955f4f86f52046deec56cf8aa0c5383584
phynnoex <bankoletheophilus21@gmail.com>
2024-07-02 23:09:22 -0400

    hmn

--- signup-project/main.js
+++ signup-project/main.js
@@ -2,6 +2,14 @@ import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {
+  axisHelper,
+  axisHelperName,
+  gridHelper,
+  gridHelperName,
+  polarGridHelper,
+  polarGridHelperName,
+} from "./helpers/helpers";
 import gsap from 'gsap';
 import ScrollTrigger from "gsap/ScrollTrigger";
 
@@ -13,6 +21,10 @@ const canvas = document.getElementById('d-canvas');
 // Basic scene setup
 const scene = new THREE.Scene();
 scene.background = null;
+
+//grid helper
+// gridHelper(scene)
+axisHelper(scene)
 
 // Camera setup
 const aspect = canvas.offsetWidth / canvas.offsetHeight;
@@ -31,21 +43,21 @@ camera.rotation.y = Math.PI / 8;
 // const box = new THREE.Mesh(boxGeometry, boxMaterial);
 // scene.add(box);
 
-// Add sphere to scene
-const sphereGeometry = new THREE.SphereGeometry(1.5, 12, 12);
-const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xffff12 });
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-sphere.position.set(2, -2, 2);
-scene.add(sphere);
 
 // Load GLTF model
 const loader = new GLTFLoader();
 let model;
+let spade;
+let club;
+let sphere;
+let diamond;
 
+// chip
 loader.load('/models/pokerassets.glb', 
   function(gltf) {
     model = gltf.scene;
     model.position.set(0.1, 0, 0.1);
+    model.scale.set(2,2,2);
     model.castShadow = true;
     model.rotation.y = 5 * Math.PI / 4;
     visitChildren(model, (child) => {
@@ -62,6 +74,93 @@ loader.load('/models/pokerassets.glb',
   }
 );
 
+//spade
+loader.load('/models/spade.glb', 
+  function(gltf) {
+    spade = gltf.scene;
+    spade.position.set(2, 5, 0.1);
+    spade.rotation.y = 3* Math.PI / 4;
+    spade.scale.set(0.5,0.5,0.5)
+    spade.castShadow = true;
+    visitChildren(model, (child) => {
+      if (child.material) {
+        child.material.depthWrite = true;
+      }
+    });
+    scene.add(spade);
+    animate();
+  },
+  undefined,
+  function(error) {
+    console.error('An error happened', error);
+  }
+);
+
+//club
+loader.load('/models/club.glb', 
+  function(gltf) {
+    club = gltf.scene;
+    club.position.set(-5, -3, 1);
+    club.rotation.y = 3* Math.PI / 4;
+    club.scale.set(1,1,1)
+    club.castShadow = true;
+    visitChildren(model, (child) => {
+      if (child.material) {
+        child.material.depthWrite = true;
+      }
+    });
+    scene.add(club);
+    animate();
+  },
+  undefined,
+  function(error) {
+    console.error('An error happened', error);
+  }
+);
+
+//sphere
+loader.load('/models/sphere.glb', 
+  function(gltf) {
+    sphere = gltf.scene;
+    sphere.position.set(-5, 2, 0);
+    sphere.rotation.y = 3* Math.PI / 4;
+    sphere.scale.set(1,1,1)
+    sphere.castShadow = true;
+    visitChildren(model, (child) => {
+      if (child.material) {
+        child.material.depthWrite = true;
+      }
+    });
+    scene.add(sphere);
+    animate();
+  },
+  undefined,
+  function(error) {
+    console.error('An error happened', error);
+  }
+);
+
+//diamond
+loader.load('/models/diamond.glb', 
+  function(gltf) {
+    diamond = gltf.scene;
+    diamond.position.set(4, -2, -1);
+    diamond.rotation.y = 4* Math.PI / 4;
+    diamond.scale.set(1,1,1)
+    diamond.castShadow = true;
+    visitChildren(model, (child) => {
+      if (child.material) {
+        child.material.depthWrite = true;
+      }
+    });
+    scene.add(diamond);
+    animate();
+  },
+  undefined,
+  function(error) {
+    console.error('An error happened', error);
+  }
+);
 // Add light to scene
 scene.add(new THREE.AmbientLight(0xffffff, 0.5));
 const light = new THREE.DirectionalLight(0xffffff, 1);
@@ -101,12 +200,33 @@ function transformequ(num) {
   return 2 * num + 3;
 }
 
+let onPassword = false;
+//input password
+let password = document.getElementById("Password");
+
+password.addEventListener("focus",function () {
+  onPassword = true;
+})
+password.addEventListener("blur",function () {
+  onPassword = false;
+})
+
+
 // Animation loop
 let step = 0;
 const animate = function() {
-  step += 0.004;
+  if (onPassword == false) {
+    step += 0.004;
+  }
   model.rotation.y = 0.5 * Math.cos(step);
   model.rotation.x = 0.5 * Math.sin(step);
+  spade.rotation.y = 0.5 * Math.cos(step);
+  diamond.rotation.y = 0.5 * Math.cos(step);
+  club.rotation.x = 0.5 * Math.cos(step);
+  club.rotation.z = 0.5 * Math.cos(step);
+  sphere.rotation.z = 0.5 * (Math.cos(step)-0.3);
+  sphere.rotation.x = 0.5 * (Math.cos(step)-0.3);
+
 
   requestAnimationFrame(animate);
   controls.update();
